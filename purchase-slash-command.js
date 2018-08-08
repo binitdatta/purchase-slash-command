d7bd97fbb76739107ee9871a4aa87b5396b21fee
Binit Datta <binitdatta@Binit-Dattas-MacBook-Pro.local>
2018-08-08 16:09:30 -0500

    first commit

---
+++ purchase-slash-command.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// allow us to easily process POST requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/purchase', async(req, res) => {
+  console.log(req.body);
+  res.send('OK');
+})
+
+const PORT = 9647;
+
+app.listen(PORT, () => {
+  console.log(`Slack Bot Server has Started at ${PORT}`);
+})
